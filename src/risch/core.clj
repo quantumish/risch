67470dfd0132da4c727335aec711ad9d14f67ba4
David Freifeld <freifeld.david@gmail.com>
2022-03-24 21:38:20 -0700

    Some more applicable boilerplate and experiments

--- src/risch/core.clj
+++ src/risch/core.clj
@@ -1,16 +1,39 @@
 (ns risch.core
   (:require [instaparse.core :as insta]))
 
-(def parse-arithmetic
-  (insta/parser
-    "S = num op num
-     num = #'[0-9]+'
-     op = ('+'|'-')
-     "))
+(def parse
+  (insta/parser "
+expr = expr op expr |  '(' expr ')' | expr expr | num | var
+var = #'[A-Za-zΑ-Ωα-ω]'
+num = #'[0-9]+'
+op = ('+'|'-'|'^'|'/'|'*')
+    "))
 
-(defn eval-parsed-arithmetic [parsed]
-  (rest parsed))
+(defn print-tree
+  ([tree] (apply str (map #(print-tree % "") tree)))
+  ([tree prefix]
+   (if (vector? tree)
+     (->> (map #(print-tree % (str prefix "-")) tree)
+          (apply str))
+     (str prefix tree "\n"))))
+
+
+(println (print-tree (parse "(12+23)^Λ+β")))
+
+(doseq [expr (parse "(12+23)^Λ+β")]
   
-(eval-parsed-arithmetic (parse-arithmetic "12+23"))
+  (println expr))
 
+(defn integrate [parsed wrt]
+  (if (not (some? (re-matches #"[A-Za-zΑ-Ωα-ω]" wrt)))
+    (throw (AssertionError. "Integral taken with respect to invalid variable!")))
+  (println "All good!"))
 
+(integrate (parse "A+B") "2")
+  ; (doseq [expr parsed] 
+         
+(def trivial-integrals
+  {:cos [:expr [:scalar -1] [:func :sin]]
+   :sin [:func :cos]})
+
+(trivial-integrals :sin)
